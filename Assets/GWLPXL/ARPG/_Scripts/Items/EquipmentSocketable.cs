d81db2b5715140fdd9b243370a6d06721698ed8e
gwl <gwlenglish@gmail.com>
2021-06-20 10:39:36 -0400

    socketable abstract
    socketable abstract, equipment socket is a type.

---
+++ Assets/GWLPXL/ARPG/_Scripts/Items/EquipmentSocketable.cs
@@ -0,0 +1,26 @@
+﻿using GWLPXL.ARPGCore.Traits.com;
+
+using UnityEngine;
+
+namespace GWLPXL.ARPGCore.Items.com
+{
+    /// <summary>
+    /// in progress
+    /// </summary>
+    [CreateAssetMenu(menuName ="GWLPXL/ARPG/Socketables/New Equipment Socketable")]
+
+    public class EquipmentSocketable : Socketable
+    {
+        public EquipmentTrait Socketable = null;
+        public override string GetUserDescription()
+        {
+            string generated = GetGeneratedItemName();
+            if (string.IsNullOrEmpty(generated) == false)
+            {
+                return GetGeneratedItemName() + Socketable.GetTraitUIDescription();
+            }
+
+            return GetBaseItemName() + Socketable.GetTraitUIDescription();
+        }
+    }
+}
--- Assets/GWLPXL/ARPG/_Scripts/Items/Socketable.cs
+++ Assets/GWLPXL/ARPG/_Scripts/Items/Socketable.cs
@@ -1,31 +1,12 @@
-﻿using GWLPXL.ARPGCore.Traits.com;
-using GWLPXL.ARPGCore.Types.com;
-
-using UnityEngine;
+﻿using GWLPXL.ARPGCore.Types.com;
 
 namespace GWLPXL.ARPGCore.Items.com
 {
-    [System.Serializable]
-    public class SocketableVars
-    {
-        public string BaseName = string.Empty;
-        public int StackingAmount = 5;
-        public string GeneratedName = string.Empty;
-        public EquipmentTrait Socketable = null;
-        public SocketableVars(EquipmentTrait socketable, string basename, int stacking = 1)
-        {
-            Socketable = socketable;
-            BaseName = basename;
-            StackingAmount = stacking;
-        }
-    }
-    /// <summary>
-    /// in progress
-    /// </summary>
-    [CreateAssetMenu(menuName ="GWLPXL/ARPG/Socketables/New Socketable")]
-    public class Socketable : Item
+
+    public abstract class Socketable : Item
     {
         public SocketableVars Vars;
+
         public override string GetBaseItemName()
         {
             return Vars.BaseName;
@@ -46,16 +27,6 @@ namespace GWLPXL.ARPGCore.Items.com
             return Vars.StackingAmount;
         }
 
-        public override string GetUserDescription()
-        {
-            string generated = GetGeneratedItemName();
-            if (string.IsNullOrEmpty(generated) == false)
-            {
-                return GetGeneratedItemName() + Vars.Socketable.GetTraitUIDescription();
-            }
-
-            return GetBaseItemName() + Vars.Socketable.GetTraitUIDescription();
-        }
 
         public override bool IsStacking()
         {
@@ -65,6 +36,20 @@ namespace GWLPXL.ARPGCore.Items.com
         public override void SetGeneratedItemName(string newName)
         {
             Vars.GeneratedName = newName;
+        }
+    }
+
+    [System.Serializable]
+    public class SocketableVars
+    {
+        public string BaseName = string.Empty;
+        public int StackingAmount = 5;
+        public string GeneratedName = string.Empty;
+
+        public SocketableVars(string basename, int stacking = 1)
+        {
+            BaseName = basename;
+            StackingAmount = stacking;
         }
     }
 }
