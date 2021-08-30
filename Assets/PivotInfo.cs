063591abab4511cfb4368afe155fee20098d7b5e
gwl <gwlenglish@gmail.com>
2021-08-30 18:22:38 -0400

    grid inventory working
    it's working, now set a script for pivot positions and clean up the imported package to integrate

---
+++ Assets/PivotInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotInfo : MonoBehaviour
+{
+    public Vector2 EquippedPivotInfo = new Vector2(.5f, .5f);
+
+}
